793d7e4db92d6d501c86870bb77be91deee100fb
Andrew Davis <andrewdavis@wattopoststudio.com>
2016-07-17 21:55:15 -0400

    router is done, unit testing is pending

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 import Koa from 'koa'
+import koa_router from 'koa-router'
 const app = new Koa()
+const router = koa_router()
 
 // response
-app.use(async (ctx) => {
-  ctx.body = 'Hello World'
+app.use(async (ctx, next) => {
+  next()
 })
+
+router.get('/', (ctx) => {
+  ctx.body = "Hello from GET /"
+})
+.get('/api', (ctx) => {
+  ctx.body = "Hello from GET /api"
+})
+.get('/api/:id', (ctx) => {
+  const { id } = ctx.params
+  ctx.body = "Hello from GET /api with :id " + id
+})
+.post('/api', (ctx) => {
+  ctx.body = "Hello from POST /api"
+})
+.put('/api/:id', (ctx) => {
+  ctx.body = "Hello from PUT /api with :id " + id
+})
+.del('/api/:id', (ctx) => {
+  ctx.body = "Hello from DEL /api with :id " + id
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3000, () => console.log('server started 3000'))
 
 export default app
-
